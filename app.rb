699785ea0829fd67e76654bf93df96497c6f5b22
Matt Zimmer <zimmermja@gmail.com>
2021-11-04 11:48:16 +0000

    implemented a session

--- app.rb
+++ app.rb
@@ -2,16 +2,25 @@ require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader' if development?
 # http://localhost:4567
+# rackup -p 4567
 
 class Battle < Sinatra::Base
+
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1name = params[:player1name]
-    @player2name = params[:player2name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1_name]
+    @player_2 = session[:player_2_name]
     erb(:play)
   end
 
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -6,13 +6,17 @@ feature 'Testing infrastructure' do
   end
 end
 
-feature 'Testing player input' do
+feature 'Testing setup' do
   scenario 'Can receive player names in a form, submit the form, and see names on-screen' do
     visit('/')
-    fill_in("player1name", with: "Red")
-    fill_in("player2name", with: "Blue")
+    fill_in("player_1_name", with: "Red")
+    fill_in("player_2_name", with: "Blue")
     click_button("Submit")
     expect(page).to have_content 'Red'
     expect(page).to have_content 'Blue'
+  end
+
+  scenario 'Displays player HP' do
+    
   end
 end
